3782e99ca995b99d3bc605608c5028312a335f15
junxuan.gxw <junxuan.gxw@dtyunxi.com>
2023-03-27 15:18:34 +0800

    feat:add something

---
+++ eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // an eslint parser allow eslint to lint typescript source code
+    parserOptions: {
+        sourceType: 'module', // 设置为 "script" (默认) 或 "module"（如果你的代码是 ECMAScript 模块)。
+        // https://eslint.bootcss.com/docs/user-guide/configuring
+    },
+    rules:{
+        
+    }
+}
